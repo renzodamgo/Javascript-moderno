67e5a3095231e4ee51fac85628e72232d1eb6434
Renzo Damian <renzo.damian.go@gmail.com>
2022-07-26 16:46:29 -0500

    Imprimir variables en consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -3,3 +3,10 @@
 let a = 10;
 // var sigue siendo válida pero es recomendable usar let porque limita el scope de la variable
 var b = 20;
+const c = 30;
+
+// Enmarcar entre paréntesis para saber que variable está imprimiendo junto con su valor
+
+console.log({ a }) // {a: 10}
+console.log({ b }) // {b: 20}
+console.log({ c }) // {c: 30}
