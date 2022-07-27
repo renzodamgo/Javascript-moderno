40eabbb0e5e6a0ef8c2bec2efaaee317a68ceb5e
Renzo Damian <renzo.damian.go@gmail.com>
2022-07-26 19:57:54 -0500

    add alert.js

--- 01-fundamentos/assets/js/app.js
+++ 01-fundamentos/assets/js/app.js
@@ -3,17 +3,18 @@
 let a = 10;
 // var sigue siendo válida pero es recomendable usar let porque limita el scope de la variable
 var b = 20;
-const c = 30;
+const c = 'Hola ';
 const d = 'Spiderman';
+const saludo = c + d;
 // Enmarcar entre paréntesis para saber que variable está imprimiendo junto con su valor
 
 console.log({ a }); // {a: 10}
 console.log({ b }); // {b: 20}
 console.log({ c }); // {c: 30}
 
-console.table({ a, b, c, d }
+console.table({ a, b, c, d })
 // (índice)	Valor
 // a					10
 // b					20
 // c					30
-// d					'Spiderman')
+// d					Spiderman
